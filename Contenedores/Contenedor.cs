964506de98317ee47c8c77fa0a2c898b5ae7bc9e
Jillazquez <117284941+Jillazquez@users.noreply.github.com>
2024-12-16 19:38:10 +0100

    Agregar archivos de proyecto.

---
+++ Contenedores/Contenedor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contenedores
+{
+    internal class Contenedor
+    {
+        private int _id { get; set; }
+        private int lat { get; set; }
+        private int lon { get; set; }
+        private int nivel { get; set; }
+        private int capacidad { get; set; }
+
+        //Constructor de los contenedores
+        public Contenedor(int id, int lat, int lon, int nivel, int capacidad)
+        {
+            this._id = id;
+            this.lat = lat;
+            this.lon = lon;
+
+            if (nivel < 0 || nivel > 100)
+                throw new ArgumentOutOfRangeException(nameof(nivel), "El nivel debe estar entre 0 y 100.");
+            this.nivel = nivel;
+
+            if (capacidad < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacidad), "La capacidad no puede ser negativa.");
+            this.capacidad = capacidad;
+        }
+
+    }
+}
---
+++ Contenedores/Program.cs
@@ -0,0 +1,29 @@
+﻿using Contenedores;
+using System.Collections;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Genero objeto random para generar numeros aleatorios
+        Random random = new Random();
+        //Consultar el numero de contenedores
+        int numContenedores = 0;
+        Console.WriteLine("Introduce con cuantos contenedores crear la simulacion");
+        numContenedores = int.Parse(Console.ReadLine());
+
+        //ArrayList para almacenar contenedores
+        ArrayList contenedores = new ArrayList();
+        for(int i = 0; i < numContenedores; i++)
+        {
+            int lat = random.Next(100)+1;
+            int lon = random.Next(100) + 1;
+            Contenedor c = new Contenedor(i, lat, lon,0,0);
+            contenedores.Add(c);
+        }
+
+        foreach (Contenedor cont in contenedores)
+        {
+
+        }
+    }
+}
